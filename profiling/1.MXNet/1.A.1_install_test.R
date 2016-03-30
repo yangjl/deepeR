d0087662d45a596305eb686e54a3396052363172
yangjl0930@gmail.com <yangjl0930@gmail.com>
2016-03-30 16:10:49 -0700

    add ipynb

---
+++ profiling/1.MXNet/1.A.1_install_test.R
@@ -0,0 +1,70 @@
+### Jinliang Yang
+### Installation and test
+
+# CPU only docker
+sudo docker run -it kaixhin/mxnet
+
+install.packages("drat", repos="https://cran.rstudio.com")
+drat:::addRepo("dmlc")
+install.packages("mxnet")
+
+
+install.packages("mlbench")
+require(mlbench)
+library("mxnet")
+
+data(Sonar, package="mlbench")
+
+Sonar[,61] = as.numeric(Sonar[,61])-1
+train.ind = c(1:50, 100:150)
+train.x = data.matrix(Sonar[train.ind, 1:60])
+train.y = Sonar[train.ind, 61]
+test.x = data.matrix(Sonar[-train.ind, 1:60])
+test.y = Sonar[-train.ind, 61]
+
+
+mx.set.seed(0)
+model <- mx.mlp(train.x, train.y, hidden_node=10, out_node=2, out_activation="softmax",
+                num.round=20, array.batch.size=15, learning.rate=0.07, momentum=0.9, 
+                eval.metric=mx.metric.accuracy)
+graph.viz(model$symbol$as.json())
+
+preds = predict(model, test.x)
+pred.label = max.col(t(preds))-1
+table(pred.label, test.y)
+
+### Regression
+data(BostonHousing, package="mlbench")
+
+train.ind = seq(1, 506, 3)
+train.x = data.matrix(BostonHousing[train.ind, -14])
+train.y = BostonHousing[train.ind, 14]
+test.x = data.matrix(BostonHousing[-train.ind, -14])
+test.y = BostonHousing[-train.ind, 14]
+
+
+# Define the input data
+data <- mx.symbol.Variable("data")
+# A fully connected hidden layer
+# data: input source
+# num_hidden: number of neurons in this hidden layer
+fc1 <- mx.symbol.FullyConnected(data, num_hidden=1)
+
+# Use linear regression for the output layer
+lro <- mx.symbol.LinearRegressionOutput(fc1)
+
+mx.set.seed(0)
+model <- mx.model.FeedForward.create(lro, X=train.x, y=train.y,
+                                     ctx=mx.cpu(), num.round=50, array.batch.size=20,
+                                     learning.rate=2e-6, momentum=0.9, eval.metric=mx.metric.rmse)
+preds = predict(model, test.x)
+sqrt(mean((preds-test.y)^2))
+demo.metric.mae <- mx.metric.custom("mae", function(label, pred) {
+    res <- mean(abs(label-pred))
+    return(res)
+})
+
+mx.set.seed(0)
+model <- mx.model.FeedForward.create(lro, X=train.x, y=train.y,
+                                     ctx=mx.cpu(), num.round=50, array.batch.size=20,
+                                     learning.rate=2e-6, momentum=0.9, eval.metric=demo.metric.mae)
