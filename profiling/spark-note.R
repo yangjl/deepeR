2fad38aa31e2a25cbdc5f1163d737a701bd6e09a
Jinliang Yang <yangjl0930@gmail.com>
2016-04-08 16:11:42 -0700

    updates

--- profiling/spark-note.R
+++ profiling/spark-note.R
@@ -1,7 +1,14 @@
-docker run -p 9000:9000 -p 4040-4045:4040-4045 andypetrella/spark-notebook:0.6.3-scala-2.11.7-spark-1.6.0-hadoop-2.2.0-with-hive-with-parquet
+#docker run -p 9000:9000 -p 4040-4045:4040-4045 andypetrella/spark-notebook:0.6.3-scala-2.11.7-spark-1.6.0-hadoop-2.2.0-with-hive-with-parquet
 
-jupyter notebook
-ssh -N -L localhost:6005:localhost:9999 farm ssh jolyang@bigmem2 -L localhost:6005:localhost:9999
+#jupyter notebook
 
 
-srun -p bigmemh --pty jupyter notebook JUPYTER_RUNTIME_DIR=/home/jolyang
+
+#srun -p bigmemh --pty jupyter notebook JUPYTER_RUNTIME_DIR=/home/jolyang
+
+
+
+set_jupyter(port=9996)
+
+
+
